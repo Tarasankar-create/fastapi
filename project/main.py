a205159c10974f6ea146c78182cdd9cf8a26b93c
Tarasankar <dastarasankar393@gmail.com>
2026-08-17 23:20:20 +0530

    Resolve error

--- project/main.py
+++ project/main.py
@@ -6,10 +6,10 @@ import json
 app=FastAPI()
 
 class Patient(BaseModel):
-    id:Annotated[str,Field(...,description="Id of the patient",examples="P001")]
+    id:Annotated[str,Field(...,description="Id of the patient",example="P001")]
     name: Annotated[str,Field(...,description="Name of the patient")]
     city: Annotated[str,Field(...,description="Name of the city")]
-    age: Annotated[str,Field(...,gt=0,lt=120,description="Age of the patient")]
+    age: Annotated[int,Field(...,gt=0,lt=120,description="Age of the patient")]
     gender: Annotated[Literal["male","female","others"],Field(...,description="Gender of the patient")]
     height: Annotated[float,Field(...,gt=0,description="Height of the patient in meters")]
     weight: Annotated[float,Field(...,gt=0,description="Weight of the patient in kgs")]
